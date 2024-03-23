9a09218eff453e6bb9a31747080bb71ecf2e2b68
Kong  Bunthoeurn <bunthoeurn.kong@igsaas.asia>
2024-03-23 08:21:39 +0700

    ci deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000
+  },
   plugins: [vue()],
   base: "./",
   resolve: {
